dc7aab8300c062e6fb16b68d416bf5e8d81f0e50
Vansh Choudhary <choudharyvansh642@gmail.com>
2024-08-31 01:00:57 +0530

    add little style in homepage

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function main() {
 
 
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
